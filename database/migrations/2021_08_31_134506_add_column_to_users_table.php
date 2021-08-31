b22c4080cd7f4de32d821f808783a164410a3501
masakikona <makisankonan@gmail.com>
2021-08-31 14:48:43 +0900

    add_profile

--- database/migrations/2021_08_31_134506_add_column_to_users_table.php
+++ database/migrations/2021_08_31_134506_add_column_to_users_table.php
@@ -14,7 +14,9 @@ class AddColumnToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('self_introduction', 500)->nullable();
+            $table->tinyInteger('sex')->default(0);
+            $table->string('img_name')->default('');
         });
     }
 
